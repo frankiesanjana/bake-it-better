db06a95b313a0d22c5a02de11d8fb520dcb00fbc
frankiesanjana <francesca.sanjana@gmail.com>
2022-09-29 13:05:27 +0000

    Feat: Add view class for Bake list in views.py

--- bakes/views.py
+++ bakes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Bake
 
-# Create your views here.
+class BakeList(generic.ListView):
+    """
+    Creates the view code to display bakes
+    """
+    model = Bake
+    queryset = Bake.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 8
