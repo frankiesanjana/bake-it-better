2afb450d4c7467679d94790da263209fd14176ba
frankiesanjana <francesca.sanjana@gmail.com>
2022-11-07 16:23:58 +0000

    Build: Add stars url path in bakes/urls.py

--- bakes/urls.py
+++ bakes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BakeList.as_view(), name='home'),
-    path('<slug:slug>/', views.BakeDetail.as_view(), name='bake-detail')
+    path('<slug:slug>/', views.BakeDetail.as_view(), name='bake-detail'),
+    path('star/<slug:slug>', views.BakeStar.as_view(), name='bake-star'),
 ]
