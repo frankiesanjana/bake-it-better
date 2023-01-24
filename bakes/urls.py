3d55576801e5c95baa9cc5ed77e9e77c26f69ef6
frankiesanjana <francesca.sanjana@gmail.com>
2023-01-24 15:32:11 +0000

    Build: Add path and adjust former paths in urls.py

--- bakes/urls.py
+++ bakes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.BakeList.as_view(), name='home'),
     path('mystarredbakes/', views.MyStarredBakes.as_view(), name='my-starred-bakes'),
     path('star/<slug:slug>', views.BakeStar.as_view(), name='bake-star'),
-    path('<slug:slug>/', views.BakeDetail.as_view(), name='bake-detail'),
-    
+    path('bake-detail/<slug:slug>/', views.BakeDetail.as_view(), name='bake-detail'),
+    path('addbake/', views.AddBake.as_view(), name='add-bake'),
 ]
