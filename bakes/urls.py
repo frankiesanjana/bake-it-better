ac2dbeff3367149a4f7d36335b8803809e83261b
frankiesanjana <francesca.sanjana@gmail.com>
2023-01-25 07:56:13 +0000

    Build: Add & test url paths to edit & delete bakes

--- bakes/urls.py
+++ bakes/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('mystarredbakes/', views.MyStarredBakes.as_view(), name='my-starred-bakes'),
     path('star/<slug:slug>', views.BakeStar.as_view(), name='bake-star'),
     path('bake-detail/<slug:slug>/', views.BakeDetail.as_view(), name='bake-detail'),
-    path('addbake/', views.AddBake.as_view(), name='add-bake'),
+    path('add-bake/', views.AddBake.as_view(), name='add-bake'),
+    path('edit-bake/<slug:slug>/', views.UpdateBake.as_view(), name='edit-bake'),
+    path('delete-bake/<slug:slug>/', views.DeleteBake.as_view(), name='delete-bake'),
 ]
