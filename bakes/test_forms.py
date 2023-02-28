19204828e8fedd1cc53e5e6431565b4ad03cb27b
frankiesanjana <francesca.sanjana@gmail.com>
2023-02-28 17:20:03 +0000

    Test: Add automated form tests in test_forms.py

--- bakes/test_forms.py
+++ bakes/test_forms.py
@@ -3,7 +3,9 @@ from .forms import BakeForm, CommentForm, BestForForm
 
 
 class TestBakeForm(TestCase):
-
+    """
+    Provides automated tests for the form BakeForm
+    """
     def test_title_is_required(self):
         form = BakeForm({'title': ''})
         self.assertFalse(form.is_valid())
@@ -32,3 +34,37 @@ class TestBakeForm(TestCase):
         form = BakeForm({'title': ''})
         self.assertFalse(form.is_valid())
         self.assertNotIn('description', form.errors.keys())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = BakeForm()
+        self.assertEqual(form.Meta.fields, ['title', 'description', 'difficulty', 'equipment', 'ingredients', 'method', 'featured_image', 'status'])
+
+
+class TestCommentForm(TestCase):
+    """
+    Provides automated tests for the form CommentForm
+    """
+    def test_body_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.fields, ('body',))
+
+
+class TestBestForForm(TestCase):
+    """
+    Provides automated tests for the form BestForForm
+    """
+    def test_best_for_is_required(self):
+        form = BestForForm({'best_for': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('best_for', form.errors.keys())
+        self.assertEqual(form.errors['best_for'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = BestForForm()
+        self.assertEqual(form.Meta.fields, ('best_for',))
