fc0092f3564706040dea2a76aa22741b53bd4347
frankiesanjana <francesca.sanjana@gmail.com>
2023-03-02 14:15:59 +0000

    Test: Restore Postgres database in settings.py

--- bakeitbetter/settings.py
+++ bakeitbetter/settings.py
@@ -111,18 +111,19 @@ WSGI_APPLICATION = 'bakeitbetter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# The commented-out databases are unused in the live version but remain here 
+# to allow for testing, since the local sqlite database is needed for tests
 
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
